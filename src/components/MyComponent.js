6e97a327fdcb6201554a827161b840336171d598
LuTanDat <ludathoc@gmail.com>
2024-09-07 22:12:15 +0700

    u-event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -11,12 +11,18 @@ class MyComponent extends React.Component {
     age: 24
   }
 
+  handleClick = (event) => {
+    console.log('Clicked me !:', event.target);
+    console.log('My name is', this.state.name);
+  }
+
   //JSX
   render() {
     return (
       <>
         <h1>Hello, world!</h1>
         My name is {this.state.name}. I'm living in {this.state.address} and now I'm {this.state.age} years old.
+        <button onClick={this.handleClick}>Click me!</button>
       </>
     )
   }
