9fa0514397ec796c68f971f8f6b2f4eb4983ee6e
LuTanDat <ludathoc@gmail.com>
2024-09-14 22:05:07 +0700

    u_customize axios

---
+++ src/services/apiServices.js
@@ -0,0 +1,19 @@
+import axios from '../utils/axiosCustomize'; // sau nay de thay doi path, cau hinh request, response
+
+
+const postAddNewUsers = async (email, password, username, role, image) => {
+  // submit data
+  const data = new FormData(); // dung cho du lieu lon nhu file, video
+  data.append('email', email);
+  data.append('password', password);
+  data.append('username', username);
+  data.append('role', role);
+  data.append('userImage', image);
+
+  return await axios.post('api/v1/participant', data)
+}
+
+export {
+  postAddNewUsers,
+
+}
---
+++ src/utils/axiosCustomize.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: 'http://localhost:8081/',
+  // timeout: 1000,
+  // headers: {'X-Custom-Header': 'foobar'} // token
+});
+
+
+//------------------------------------------------------------------------------
+
+/**
+ * component -> axios config -> request 
+ * response <- axios config <-
+ * tuong tu nhu middleware
+ */
+
+// Add a request interceptor
+instance.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+// Add a response interceptor
+instance.interceptors.response.use(function (response) {
+  console.log('>>> check res interceptor: ', response);
+
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response && response.data ? response.data : response;
+}, function (error) {
+  console.log('>>> check res interceptor: ', error);
+
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return error && error.response && error.response.data
+    ? error.response.data : Promise.reject(error);
+})
+
+export default instance
