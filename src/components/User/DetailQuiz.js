3d0829864f5a0c174b07308ecaeb4dbd901ca7ba
LuTanDat <ludathoc@gmail.com>
2024-09-18 08:50:13 +0700

    u_process data

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
+import _ from 'lodash';
 
 const DetailQuiz = () => {
   const params = useParams();
@@ -12,7 +13,33 @@ const DetailQuiz = () => {
 
   const fetchDataQuiz = async () => {
     let res = await getDataQuiz(quizId);
-    console.log(res);
+    console.log('data backend: ', res);
+    if (res && res.EC === 0) {
+      let raw = res.DT; // du lieu goc return from backend
+      let data = _.chain(raw)
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+          let answers = [];
+          let questionDescription, image = null;
+
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDescription = item.description;
+              image = item.image;
+            }
+            answers.push(item.answers)
+            // console.log('item answers: ', item.answers);
+          })
+          // console.log('value: ', value, ' key: ', key)
+
+          return { questionId: key, answers, questionDescription, image }
+        })
+        .value()
+
+      console.log('data: ', data)
+    }
   }
 
   return (
