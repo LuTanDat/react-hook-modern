faa81a5ffa69550fc67c7c705aa567becbbd4e2a
LuTanDat <ludathoc@gmail.com>
2024-09-27 17:21:49 +0700

    u_ dap an + customize lat vat

--- src/components/Admin/Content/Quiz/ManageQuiz.js
+++ src/components/Admin/Content/Quiz/ManageQuiz.js
@@ -8,6 +8,8 @@ import TableQuiz from './TableQuiz';
 import Accordion from 'react-bootstrap/Accordion';
 import QuizQA from './QuizQA';
 import AssignQuiz from './AssignQuiz';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 const options = [
   { value: 'EASY', label: 'EASY' },
@@ -55,98 +57,82 @@ const ManageQuiz = () => {
   return (
     <div className="quiz-container">
 
-      <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>
-            <div className="title">
-              Manage Quizzes
-            </div>
-          </Accordion.Header>
-          <Accordion.Body>
-            <div className="add-new">
-              <fieldset className="border rounded-3 p-3">
-                <legend className="float-none w-auto px-3">Add New Quiz:</legend>
-                <div className="form-floating mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Your's name Quiz"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                  <label >Name</label>
-                </div>
-                <div className="form-floating">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                  />
-                  <label >Description</label>
-                </div>
-                <div className='my-3'>
-                  <Select
-                    defaultValue={type}
-                    onChange={setType}
-                    options={options}
-                    placeholder={'Quiz type....'}
-                  />
-                </div>
-                <div className='more-actions form-group'>
-                  <label htmlFor='labelUpload' className="form-label label-upload">
-                    <FcPlus /> Upload Image
-                  </label>
-                  <input
-                    id='labelUpload' type='file' className='form-control' hidden
-                    onChange={(e) => handleChangeFile(e)}
-                  />
-                </div>
-                <div className='img-preview'>
-                  {previewImg ?
-                    <img src={previewImg} alt="img" /> :
-                    <div>Preview Image</div>
-                  }
-                </div>
-                <div className='mt-3'>
-                  <button
-                    className='btn btn-warning'
-                    onClick={() => handleSubmitCreateQuiz()}
-                  >Save</button>
-                </div>
-              </fieldset>
-            </div>
-            <hr />
-            <div className="list-detail mt-2">
-              <TableQuiz
-                isCreatedNewQuiz={isCreatedNewQuiz}
-                setIsCreatedNewQuiz={setIsCreatedNewQuiz}
-              />
-            </div>
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>
-            <div className="title">
-              Update Q/A Quizzes
-            </div>
-          </Accordion.Header>
-          <Accordion.Body>
-            <QuizQA />
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="2">
-          <Accordion.Header>
-            <div className="title">
-              Assign Quiz to User
-            </div>
-          </Accordion.Header>
-          <Accordion.Body>
-            <AssignQuiz />
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
+      <Tabs
+        defaultActiveKey="home"
+        id="uncontrolled-tab-example"
+        className="mb-2"
+        justify
+      >
+        <Tab eventKey="home" title="Manage Quizzes">
+          <div className="add-new">
+            <fieldset className="border rounded-3 p-3">
+              <legend className="float-none w-auto px-3">Add New Quiz:</legend>
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Your's name Quiz"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <label >Name</label>
+              </div>
+              <div className="form-floating">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+                <label >Description</label>
+              </div>
+              <div className='my-3'>
+                <Select
+                  defaultValue={type}
+                  onChange={setType}
+                  options={options}
+                  placeholder={'Quiz type....'}
+                />
+              </div>
+              <div className='more-actions form-group'>
+                <label htmlFor='labelUpload' className="form-label label-upload">
+                  <FcPlus /> Upload Image
+                </label>
+                <input
+                  id='labelUpload' type='file' className='form-control' hidden
+                  onChange={(e) => handleChangeFile(e)}
+                />
+              </div>
+              <div className='img-preview'>
+                {previewImg ?
+                  <img src={previewImg} alt="img" /> :
+                  <div>Preview Image</div>
+                }
+              </div>
+              <div className='mt-3'>
+                <button
+                  className='btn btn-warning'
+                  onClick={() => handleSubmitCreateQuiz()}
+                >Save</button>
+              </div>
+            </fieldset>
+          </div>
+          <hr />
+          <div className="list-detail mt-2">
+            <TableQuiz
+              isCreatedNewQuiz={isCreatedNewQuiz}
+              setIsCreatedNewQuiz={setIsCreatedNewQuiz}
+            />
+          </div>
+        </Tab>
+        <Tab eventKey="profile" title="Update Q/A Quizzes">
+          <QuizQA />
+        </Tab>
+        <Tab eventKey="contact" title="Assign Quiz to User">
+          <AssignQuiz />
+        </Tab>
+      </Tabs>
     </div>
   )
 }
--- src/components/Admin/Sidebar.js
+++ src/components/Admin/Sidebar.js
@@ -1,4 +1,4 @@
-import 'react-pro-sidebar/dist/css/styles.css';
+// import 'react-pro-sidebar/dist/css/styles.css';
 import {
   ProSidebar,
   Menu,
