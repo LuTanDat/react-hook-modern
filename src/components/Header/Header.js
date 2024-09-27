8b090cf3d4178a83c8258fad853ab3d9be775e3a
LuTanDat <ludathoc@gmail.com>
2024-09-27 16:50:47 +0700

    u_profile user v1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,10 +9,14 @@ import { toast } from 'react-toastify';
 import { doLogout } from '../../redux/action/userAction';
 import Language from './Language';
 import { FaReact } from "react-icons/fa";
+import { useState } from 'react';
+import Profile from './Profile';
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const [show, setShow] = useState(false);
 
   const isAuthenticated = useSelector(state => state.user.isAuthenticated);
   const account = useSelector(state => state.user.account);
@@ -37,44 +41,51 @@ const Header = () => {
   }
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <NavLink to="/" className='navbar-brand'>
-          <FaReact className="brand-icon" />
-          React-Bootstrap
-        </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavLink to="/" className='nav-link'>Home</NavLink>
-            <NavLink to="/users" className='nav-link'>Users</NavLink>
-            <NavLink to="/admin" className='nav-link'>Admin</NavLink>
-            {/* <Nav.Link href="/">Home</Nav.Link>
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <NavLink to="/" className='navbar-brand'>
+            <FaReact className="brand-icon" />
+            React-Bootstrap
+          </NavLink>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavLink to="/" className='nav-link'>Home</NavLink>
+              <NavLink to="/users" className='nav-link'>Users</NavLink>
+              <NavLink to="/admin" className='nav-link'>Admin</NavLink>
+              {/* <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/users">Users</Nav.Link>
             <Nav.Link href="/admin">Admin</Nav.Link> */}
-          </Nav>
-          <Nav>
-            {!isAuthenticated ?
-              <>
-                <button className='btn-login' onClick={() => handleLogin()}>Log in</button>
-                <button className='btn-signup' onClick={() => handleSignup()}> Sign up</button>
-              </>
-              :
-              <>
-                {/* <span>Hello {account.username}</span> */}
-                <NavDropdown title="Setting" id="basic-nav-dropdown">
-                  <NavDropdown.Item >Profile</NavDropdown.Item>
-                  <NavDropdown.Item onClick={() => handleLogout()}>Log out</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item >Other</NavDropdown.Item>
-                </NavDropdown>
-              </>
-            }
-            <Language />
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar >
+            </Nav>
+            <Nav>
+              {!isAuthenticated ?
+                <>
+                  <button className='btn-login' onClick={() => handleLogin()}>Log in</button>
+                  <button className='btn-signup' onClick={() => handleSignup()}> Sign up</button>
+                </>
+                :
+                <>
+                  {/* <span>Hello {account.username}</span> */}
+                  <NavDropdown title="Setting" id="basic-nav-dropdown">
+                    <NavDropdown.Item onClick={() => setShow(true)}>Profile</NavDropdown.Item>
+                    <NavDropdown.Item onClick={() => handleLogout()}>Log out</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item >Other</NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              }
+              <Language />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar >
+      <Profile
+        show={show}
+        setShow={setShow}
+      />
+    </>
+
   );
 }
 
