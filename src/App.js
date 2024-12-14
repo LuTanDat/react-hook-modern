9af5b9257413ea72f68021effa979184b26764ca
LuTanDat <ludathoc@gmail.com>
2024-12-14 10:29:16 +0700

    u_bootstrap_router_v2

--- src/App.js
+++ src/App.js
@@ -1,47 +1,21 @@
-// import logo from './logo.svg';
-// import './App.scss';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-// import MyComponent from './components/MyComponent';
-
-// import React from 'react';
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className='app-container'>
-//         <MyComponent />
-//       </div>
-//     );
-//   }
-// }
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello world !
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
-
-//-------------------------------------------------------------------------------
 import './App.scss';
+import { Outlet } from "react-router-dom";
+import Header from './components/Header/Header';
 
 const App = () => {
 
   return (
     <div className="app-container">
-      Hello world!
+      <div className='header-container'>
+        <Header />
+      </div>
+      <div className='main-container'>
+        <div className='sidenav-container'>
+        </div>
+        <div className='app-content'>
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,7 @@
+const Home = (props) => {
+  return (
+    <div className="homepage-container">Home component</div>
+  )
+}
+
+export default Home;
