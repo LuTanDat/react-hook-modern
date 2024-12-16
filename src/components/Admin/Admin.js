bea475bfed611f87274d3c2a81ead7adc7d16e68
LuTanDat <ludathoc@gmail.com>
2024-12-16 21:37:59 +0700

    u_format structure

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -3,9 +3,6 @@ import './Admin.scss'
 import Sidebar from "./Sidebar";
 import { FaBars } from 'react-icons/fa';
 import { Outlet } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import 'react-toastify/dist/ReactToastify.css';
 
 const Admin = (props) => {
   const [collapsed, setCollapsed] = useState(false)
@@ -16,35 +13,12 @@ const Admin = (props) => {
       </div>
       <div className="admin-content">
         <div className='admin-header'>
-          <span onClick={() => setCollapsed(!collapsed)}>
-            <FaBars className='leftside' />
-          </span>
-          <div className='rightside'>
-            <NavDropdown title="Setting" id="basic-nav-dropdown">
-              <NavDropdown.Item >Profile</NavDropdown.Item>
-              <NavDropdown.Item >Log out</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item >Other</NavDropdown.Item>
-            </NavDropdown>
-          </div>
+          <FaBars onClick={() => setCollapsed(!collapsed)} />
         </div>
         <div className='admin-main'>
           <Outlet />
         </div>
       </div>
-
-      <ToastContainer
-        position="top-right"
-        autoClose={900}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
     </div>
   )
 }
