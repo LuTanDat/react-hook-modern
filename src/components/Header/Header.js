b50df5bd975bd65d79a3cf68591a0cc3de4dc2e6
LuTanDat <ludathoc@gmail.com>
2024-12-17 18:14:00 +0700

    u_login-redux

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,15 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { NavLink, useNavigate } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 const Header = () => {
   const navigate = useNavigate();
+
+  const isAuthenticated = useSelector(state => state.user.isAuthenticated);
+  const account = useSelector(state => state.user.account);
+
+  console.log(isAuthenticated, account);
 
   const handleLogin = () => {
     navigate('/login');
@@ -30,15 +36,24 @@ const Header = () => {
             <Nav.Link href="/admin">Admin</Nav.Link> */}
           </Nav>
           <Nav>
-            <button className='btn-login' onClick={() => handleLogin()}>Log in</button>
-            <button className='btn-signup' onClick={() => handleSignup()}> Sign up</button>
-            {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
-              <NavDropdown.Item >Log in</NavDropdown.Item>
-              <NavDropdown.Item >Log out</NavDropdown.Item>
-              <NavDropdown.Item >Profile</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item >Other</NavDropdown.Item>
-            </NavDropdown> */}
+            {!isAuthenticated ?
+              <>
+                <button className='btn-login' onClick={() => handleLogin()}>Log in</button>
+                <button className='btn-signup' onClick={() => handleSignup()}> Sign up</button>
+              </>
+              :
+              <>
+                <span>Hello {account.username}</span>
+                <NavDropdown title="Setting" id="basic-nav-dropdown">
+                  <NavDropdown.Item >Log out</NavDropdown.Item>
+                  <NavDropdown.Item >Profile</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item >Other</NavDropdown.Item>
+                </NavDropdown>
+              </>
+            }
+
+
           </Nav>
         </Navbar.Collapse>
       </Container>
---
+++ src/redux/reducer/userReducer.js
@@ -0,0 +1,20 @@
+
+import { FETCH_USER_LOGIN_SUCCESS } from '../action/userAction';
+
+const INITIAL_STATE = {
+  account: {},
+  isAuthenticated: false,
+
+};
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case FETCH_USER_LOGIN_SUCCESS:
+      return {
+        ...state, account: action?.payload?.DT, isAuthenticated: true,
+      };
+
+    default: return state;
+  }
+};
+
+export default userReducer;
