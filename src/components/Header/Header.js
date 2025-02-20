0c09503735a9afb92863aa187419571a758f424c
LuTanDat <ludathoc@gmail.com>
2025-02-20 22:24:52 +0700

    u

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,8 +20,6 @@ const Header = () => {
   const isAuthenticated = useSelector(state => state.user.isAuthenticated);
   const account = useSelector(state => state.user.account);
 
-  console.log(isAuthenticated, account);
-
   const handleLogin = () => {
     navigate('/login');
   }
