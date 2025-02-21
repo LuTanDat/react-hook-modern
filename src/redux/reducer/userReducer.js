1827848fc597e915fef8d5a5794f6ae495a82fdb
LuTanDat <ludathoc@gmail.com>
2025-02-21 13:59:31 +0700

    add refresh token

--- src/redux/reducer/userReducer.js
+++ src/redux/reducer/userReducer.js
@@ -1,21 +1,56 @@
 
-import { FETCH_USER_LOGIN_SUCCESS, USER_LOGOUT_SUCCESS } from '../action/userAction';
+import {
+  FETCH_USER_LOGIN_SUCCESS, USER_LOGOUT_SUCCESS,
+  USER_REFRESH_TOKEN_SUCCESS,
+
+} from '../action/userAction';
 
 const INITIAL_STATE = {
-  account: {},
+  account: {
+    access_token: '',
+    email: '',
+    username: '',
+    role: '',
+    image: ''
+  },
   isAuthenticated: false,
-
 };
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FETCH_USER_LOGIN_SUCCESS:
       return {
-        ...state, account: action?.payload?.DT, isAuthenticated: true,
+        ...state,
+        account: {
+          access_token: action?.payload?.DT.access_token,
+          email: action?.payload?.DT.email,
+          username: action?.payload?.DT.username,
+          role: action?.payload?.DT.role,
+          image: action?.payload?.DT.image,
+        },
+        isAuthenticated: true,
       };
 
     case USER_LOGOUT_SUCCESS:
       return {
-        ...state, account: {}, isAuthenticated: false,
+        ...state,
+        account: {
+          access_token: '',
+          email: '',
+          username: '',
+          role: '',
+          image: ''
+        },
+        isAuthenticated: false,
+      };
+
+    case USER_REFRESH_TOKEN_SUCCESS:
+      return {
+        ...state,
+        account: {
+          ...state.account,
+          access_token: action?.payload
+        },
+        isAuthenticated: true,
       };
 
     default: return state;
--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -9,6 +9,7 @@
 import axios from "axios";
 import NProgress from 'nprogress' // loading bars
 import { store } from '../redux/store' // get state redux outside component
+import { doLogout, refreshToken } from '../redux/action/userAction';
 
 
 NProgress.configure({
@@ -30,6 +31,24 @@ const instance = axios.create({
   // headers: {'X-Custom-Header': 'foobar'} // token
 });
 
+// Hàm gọi API refresh token
+const refreshAccessToken = async () => {
+  console.log("Redux state hiện tại:", store.getState());
+
+  const email = store?.getState()?.user?.account?.email;
+  try {
+    const res = await instance.post("v1/api/refresh-token", { email }); // Gọi API lấy access token mới
+    if (res.EC === 0) {
+      store.dispatch(refreshToken(res.DT.access_token)); // Cập nhật access token mới vào Redux
+      return res.DT.access_token; // Trả về access token mới
+    }
+  } catch (error) {
+    console.error("Lỗi khi refresh token:", error);
+    store.dispatch(doLogout()); // Nếu refresh thất bại, đăng xuất người dùng
+  }
+  return null;
+};
+
 
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
@@ -37,7 +56,9 @@ instance.interceptors.request.use(function (config) {
   NProgress.start();
 
   const access_token = store?.getState()?.user?.account?.access_token;
-  config.headers["Authorization"] = `Bearer ${access_token}`;
+  if (access_token) {
+    config.headers["Authorization"] = `Bearer ${access_token}`;
+  }
 
   return config;
 }, function (error) {
@@ -49,13 +70,25 @@ instance.interceptors.request.use(function (config) {
 instance.interceptors.response.use(function (response) {
   // Do something before response is returned
   NProgress.done();
-  console.log('>>> check res interceptor: ', response);
+  console.log('>>> check res interceptor success: ', response);
 
   return response && response.data ? response.data : response;
-}, function (error) {
+}, async (error) => {
   // Do something with response error
   NProgress.done();
-  console.log('>>> check res interceptor: ', error);
+  console.log('>>> check res interceptor error: ', error);
+
+  if (error.response) {
+    const { data, config } = error.response;
+    if (data.EC === 4 && !config._retry) { // Nếu lỗi là Access Token hết hạn và chưa thử refresh
+      config._retry = true; // Đánh dấu request này đã được thử refresh
+      const newAccessToken = await refreshAccessToken(); // Gọi hàm refresh token
+      if (newAccessToken) {
+        config.headers["Authorization"] = `Bearer ${newAccessToken}`; // Gán access token mới
+        return instance(config); // Gửi lại request với token mới
+      }
+    }
+  }
 
   return error && error.response && error.response.data
     ? error.response.data : Promise.reject(error); // less app death
